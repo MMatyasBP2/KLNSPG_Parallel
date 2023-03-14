8f274d9d5621cc1d13a2a049a95fdb534f2f98bd
MMatyasBP2 <matyika123456@gmail.com>
2023-03-15 00:32:13 +0100

    Bluetooth connection device discovering method generated

---
+++ BluetoothConnector/BluetoothConnector/MainWindow.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BluetoothConnector
+{
+    public partial class MainWindow : Form
+    {
+        Communication com = new Communication();
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private async void SearchButton_Click(object sender, EventArgs e)
+        {
+            DevicesBox.Items.Add(await com.DiscoverDevices());
+        }
+    }
+}

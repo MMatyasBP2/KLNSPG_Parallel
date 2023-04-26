15c4488b896fcc2745aa1a70df0751dd753330b8
MMatyasBP2 <matyika123456@gmail.com>
2023-04-26 11:32:37 +0200

    Exception handling added to python script

--- NumericalAnalysis/plot_results.py
+++ NumericalAnalysis/plot_results.py
@@ -10,21 +10,22 @@ def read_results(filename):
 
 
 def main():
-    sequential_times = read_results("sequential_results.txt")
-    parallel_times = read_results("parallel_results.txt")
+    try:
+        sequential_times = read_results("sequential_results.txt")
+        parallel_times = read_results("parallel_results.txt")
 
-    x = list(range(1, len(sequential_times) + 1))
+        x = list(range(1, len(sequential_times) + 1))
+        plt.plot(x, sequential_times, label="Sequential", marker="o")
+        plt.plot(x, parallel_times, label="Parallel", marker="x")
 
-    plt.plot(x, sequential_times, label="Sequential", marker="o")
-    plt.plot(x, parallel_times, label="Parallel", marker="x")
+        plt.xlabel("Method")
+        plt.ylabel("Time (s)")
+        plt.title("Sequential vs Parallel Execution Time")
+        plt.legend()
 
-    plt.xlabel("Method")
-    plt.ylabel("Time (s)")
-    plt.title("Sequential vs Parallel Execution Time")
-    plt.legend()
-
-    plt.show()
-
+        plt.show()
+    except:
+        print("Wrong file content! Please check your files!")
 
 if __name__ == "__main__":
     main()
